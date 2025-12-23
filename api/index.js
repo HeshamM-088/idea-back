fd9af600cadd093f58e40655adb85cb35d5a5ecc
HeshamM-088 <heshamkhalil1988@gmail.com>
2025-12-24 01:17:58 +0200

    vercel serverless config2

--- api/index.js
+++ api/index.js
@@ -1,3 +1,36 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "Idea Refiner API is running 🚀" });
-}
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const ideaRoutes = require("./routes/idea.routes");
+const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/v1/ideas", ideaRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Idea Refiner API is running 🚀" });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found ❌",
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Something went wrong",
+  });
+});
+
+module.exports = app;
